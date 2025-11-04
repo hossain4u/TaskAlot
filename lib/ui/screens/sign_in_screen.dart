a3e66fc85b60919825a131f78c35461e72d30c2a
hossain4u <sujonmazi20@gmail.com>
2025-11-04 19:02:39 +0600

    completed sign in screen ui design

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_project/ui/widgets/screen_background.dart';
 
@@ -19,7 +20,7 @@ class _SignInScreenState extends State<SignInScreen> {
             spacing: 8,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height:70),
+              SizedBox(height: 100),
               Text(
                 'Get started with',
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
@@ -29,8 +30,43 @@ class _SignInScreenState extends State<SignInScreen> {
               TextFormField(decoration: InputDecoration(hintText: 'Password')),
               SizedBox(height: 8),
               FilledButton(
-                onPressed: () {},
-                child: Icon(Icons.arrow_circle_right_outlined,size:25,),
+                onPressed: _onTapSignInButton,
+                child: Icon(Icons.arrow_circle_right_outlined, size: 25),
+              ),
+              SizedBox(height: 25),
+              Center(
+                child: Column(
+                  children: [
+                    TextButton(
+                      onPressed: _onTapForgetButton,
+                      child: Text('Forget Password?',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        text: "Don't have an account? ",
+                        children: [
+                          TextSpan(
+                            style: TextStyle(color: Colors.green),
+                            text: 'Sign Up',
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = _onTapSignUpButton
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
@@ -38,4 +74,10 @@ class _SignInScreenState extends State<SignInScreen> {
       ),
     );
   }
+}
+
+void _onTapSignInButton(){}
+void _onTapForgetButton(){}
+void _onTapSignUpButton(){
+
 }
